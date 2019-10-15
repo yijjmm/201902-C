1e221e4f5cc35fd6a90a7433d317db305ec60895
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-10-15 14:48:55 +0900

    Update 191015 학점출력.c

--- 191015 학점출력.c
+++ 191015 학점출력.c
@@ -31,3 +31,26 @@ int main () {
     
     return 0;
 }
+
+
+// #include <stdio.h>
+
+// int main () {
+
+// 	int input;
+
+// 	printf("양수를 입력하시오.\n");
+// 	scanf("%d", &input);
+		
+	
+// 	if (input % 2 == 0 && input >= 0)
+// 		printf("입력한 수 양수 %d는 짝수입니다.\n", input);
+	
+// 	else if (input % 2 != 0 && input >= 0)
+// 			printf("입력한 수 양수 %d는 홀수입니다.\n", input);
+
+// 	else
+// 		printf("입력한 수 %d는 음수입니다.\n", input);
+
+// 	return 0;
+// }
