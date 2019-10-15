07037bbe4d662171f342fc4e7d0b6588e092bef7
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-10-15 15:17:24 +0900

    Create 191005 switch.c

---
+++ 191005 switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main () {
+
+	int input;
+
+	printf("영어로 인사하기\n");
+	printf("아침인사-1, 점심인사-2, 저녁인사-3\n");
+	scanf("%d", &input);
+
+	switch(input){
+	case 1 :
+		printf("Good morning\n");
+		break;
+
+	case 2:
+		printf("Good afternoon\n");
+		break;
+
+	case 3:
+		printf("Good evening\n");
+		break;
+
+	}
+
+	return 0;
+}
