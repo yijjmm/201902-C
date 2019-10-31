f71b91284cbea52502af64dffa2b466a7c8efe92
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-10-31 20:08:26 +0900

    Create 191029 소수 출력.c

---
+++ 191029 소수 출력.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+
+int main(void){
+
+	int i, j;
+
+	for (i=2; i<=100; i++){
+
+		for (j=2; j<=i; j++){
+
+			if (i % j == 0){
+				break;
+			}
+		}
+			
+		if (i == j){
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
