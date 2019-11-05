5bd193a805ccf3d12fc0cae6f97c6d59744eac39
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-11-05 16:57:35 +0900

    Update 191105 정수값과 주소.c

--- 191105 정수값과 주소.c
+++ 191105 정수값과 주소.c
@@ -6,8 +6,8 @@ int main(void){
 	printf("정수 값 1개 입력\n");
 	scanf("%d", &a);
 
-	printf("정수의 값(a)은 %d\n", a);
-	printf("정수의 주소(&a)는 %d\n", &a);
+	printf("정수의 값(a)은 %d\n", a); //데이터값
+	printf("정수의 주소(&a)는 %d\n", &a); //주소
 
 	return 0;
 
