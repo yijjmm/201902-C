d936ee5ea17e405a16d0a3456634f3a2688ed7d8
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-12-03 15:44:40 +0900

    Create 2019 기말.c

---
+++ 2019 기말.c
@@ -0,0 +1,16 @@
+/* 양의 정수를 입력받아 세제곱거듭근을 소수3자리까지 구하기 */
+
+#include <stdio.h>
+
+int main(){
+float a, n;
+scanf("%f", &n);
+
+for (a=0; a<n; a+=0.001){
+    if (a * a * a >= n){
+        printf("%.3f", a - 0.001);
+        break;
+    }
+}
+return 0;
+}
